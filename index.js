43400fd8c755f9c6a9fbdffdbce2282f40f4880d
Mkashif768 <145900041+Mkashif768@users.noreply.github.com>
2024-02-25 14:12:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// task 1
+// function kashi(){
+//     const userName =prompt("enter your name");
+//     alert("wellcome:" +  userName)
+// };
+// kashi();
+//TASK 2
+// function color(){
+// const colorNmae=["red", 'green','yellow','blue'];
+// const colo = Math.floor(Math.random()* colorNmae.length);
+// const boo = colorNmae[colo];
+
+// document.write(boo)
+// };
+
+// color();
+
+
+//task 3
+// function rndnumb(){
+// const num = Math.ceil(Math.random()*100)
+// document.write(num)};
+// rndnumb();
